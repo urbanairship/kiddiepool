5cf9f5135316ce8db577e9e9eecf524c552bf426
Le Lucky <luckythetourist@gmail.com>
2012-02-14 04:46:13 +0800

    v0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='kiddiepool',
     url='https://github.com/urbanairship/kiddiepool',
-    version='0.1.0',
+    version='0.1.1',
     license='Apache',
     author='Michael Schurter',
     author_email='schmichael@urbanairship.com',
