b9dad547fc472b687dad81a397f66196adfe2b2e
Russell Mayhew <russell.mayhew@urbanairship.com>
2013-08-26 16:15:51 -0700

    Version bump to 0.3.0 with TidePool

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='kiddiepool',
     url='https://github.com/urbanairship/kiddiepool',
-    version='0.2.1',
+    version='0.3.0',
     license='Apache',
     author='Michael Schurter',
     author_email='schmichael@urbanairship.com',
