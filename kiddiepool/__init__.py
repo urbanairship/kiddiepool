16a84a8f6830d42f061c8b2593898a934618f01f
Russell Mayhew <russell.mayhew@urbanairship.com>
2014-04-14 17:39:18 -0700

    Added errors to __init__.py

--- kiddiepool/__init__.py
+++ kiddiepool/__init__.py
@@ -1,8 +1,26 @@
 from .connection import KiddieConnection
 from .pool import KiddiePool, TidePool
+from .exceptions import (
+    KiddieException,
+    KiddiePoolEmpty,
+    KiddiePoolMaxAttempts,
+    KiddieSocketError,
+    KiddieConnectionSendFailure,
+    KiddieConnectionRecvFailure,
+    KiddieClientSendFailure,
+    KiddieClientRecvFailure,
+)
 
 __all__ = [
     'KiddieConnection',
     'KiddiePool',
     'TidePool',
+    'KiddieException',
+    'KiddiePoolEmpty',
+    'KiddiePoolMaxAttempts',
+    'KiddieSocketError',
+    'KiddieConnectionSendFailure',
+    'KiddieConnectionRecvFailure',
+    'KiddieClientSendFailure',
+    'KiddieClientRecvFailure',
 ]
